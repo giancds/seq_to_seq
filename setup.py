835d449956255a5928506837f4e2d1528d871fe9
gian <giancds@gmail.com>
2015-10-09 14:32:19 +0100

    Changed numpy random to generate the same kind of parameters as theano floatX size.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
       name='Sequence-to-Sequence',
-      version='0.0.0.3',
+      version='0.0.0.4',
       description='Experiment with Sequence to Sequence Learning with Neural Networks',
       author='Giancarlo Salton',
       author_email='giancarlo.salton@mydit.ie',
