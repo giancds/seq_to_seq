9318722151c3fb17cd77f67686fb2daa1b2677d2
gian <giancds@gmail.com>
2015-10-12 13:46:39 +0100

    Minor fix on setup.py.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages
 
 setup(
       name='Sequence-to-Sequence',
-      version='0.0.0.6',
+      version='0.0.0.7',
       description='Experiment with Sequence to Sequence Learning with Neural Networks',
       author='Giancarlo Salton',
       author_email='giancarlo.salton@mydit.ie',
       url='https://github.com/giancds/seq_to_seq',
       license='MIT',
-      install_requires=['theano', 'numpy'],
+      install_requires=['theano', 'numpy', 'h5py'],
       packages=find_packages())
